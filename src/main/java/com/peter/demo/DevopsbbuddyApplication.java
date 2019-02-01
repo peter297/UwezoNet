53db58ae408dd9928fc2363d6611f4164b47d72d
peter297 <peterkimani297@gmail.com>
2019-02-01 15:06:08 +0300

    Initial commit

---
+++ src/main/java/com/peter/demo/DevopsbbuddyApplication.java
@@ -0,0 +1,14 @@
+package com.peter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbbuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsbbuddyApplication.class, args);
+    }
+
+}
+
